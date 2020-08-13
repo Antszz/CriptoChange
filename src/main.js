1731e71757236008f7455a98675c110068104780
Eduardo Sanchez <esanchezh@unsa.edu.pe>
2020-08-13 13:42:19 -0500

    filtros

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
+import router from "@/router";
+import { dollarFilert, percentFilter } from "@/filter";
+
+Vue.filter("dollar", dollarFilert);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount("#app");
